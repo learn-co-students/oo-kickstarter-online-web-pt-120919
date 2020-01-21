fd30ea176d51126d3a71eedf51b2dd912254ba25
Nguyen Ha <nynha0526@gmail.com>
2020-01-21 03:19:57 +0000

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,1 +1,14 @@
-bob = Backer.now("Bob")
+class Backer
+  attr_reader :name, :backed_projects
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project)
+    backed_projects << project
+    project.backers << self
+  end
+
+end
